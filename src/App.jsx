10a67dc1773da97d64e23dfe25135df3e409d517
Allay Sahoo <allaysahoo@gmail.com>
2024-02-09 11:34:37 +0530

    User can add items to cart, state logic is used correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [cartList, setCartList] = useState([]);
-  const [newGrocery, setNewGrocery] = useState({ groceryName: '', quantity: 0 });
 
-  const itemList = ["Bread", "Milk"];
+  const [clickedItemIndex, setClickedItemIndex] = useState(null);
+  const [cart , setCart] = useState([]);
+  const itemList = ['Bread', 'Milk', 'Butter', 'Jam', 'Egg'];
+
+  var cartItem = {name :'',quantity:0};
+
+  function handleChange(e,item)
+  {
+      cartItem.name = item; 
+      cartItem.quantity = Number(e.target.value);
+      console.log(cartItem);
+  }
+
+  function handleAddToCart() {
+    const updatedCart = [...cart]; 
+    const existingItemIndex = updatedCart.findIndex(
+      (item) => item.name === cartItem.name
+    );
+  
+    if (existingItemIndex !== -1) {
+      updatedCart[existingItemIndex].quantity = cartItem.quantity;
+    } else {
+      updatedCart.push(cartItem);
+    }
+  
+    setCart(updatedCart); 
+    console.log(updatedCart); 
+  }
 
   return (
     <div>
-      {itemList.map((item) => (
-        <li>{item}<input
-            type="number"
-            onChange={(e) => {
-              setNewGrocery({  //change name
-                groceryName: item,
-                quantity: e.target.value
-              });
-            }}
-          />
-          <button onClick={() => setCartList([...cartList, newGrocery])}>
-            Add To Cart
-          </button>
-        </li>
-      ))}
-      <ul>
-        {cartList.map((item, index) => (
-          <li key={index}>
-            {item.groceryName}: {item.quantity}
+      {itemList.map((item, index) => (
+        <div key={index}>
+          <li onClick={() => setClickedItemIndex(index)}>
+            {item}
           </li>
-        ))}
-      </ul>
+          {clickedItemIndex === index && (
+            <div>
+              <input type="number" onChange={(e)=>handleChange(e,item)}/>
+              <button onClick={handleAddToCart}>Add To Cart</button>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
